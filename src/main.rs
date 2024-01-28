a8b5a17ff09c84ac2ff75bd47c55ffb54075d168
fabrlyn <robert.barlin@gmail.com>
2024-01-28 21:20:51 +0100

    ArgOrStdin working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,64 @@
-use std::{ops::Sub, str::FromStr};
+use std::{
+    io::{self, stdin},
+    ops::Sub,
+    str::FromStr,
+};
 
 use clap::{command, Args, Parser, Subcommand};
 use collatz::Number;
 use num::BigUint;
 
-fn parse_number(s: &str) -> Result<Number, String> {
-    BigUint::from_str(s)
-        .ok()
-        .and_then(|number| Number::new(number))
-        .ok_or("Not a valid number, must be a positive integer".to_string())
+#[derive(Clone, Debug)]
+struct NumberArg(Number);
+
+impl FromStr for NumberArg {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        BigUint::from_str(s.trim())
+            .ok()
+            .and_then(|number| Number::new(number))
+            .ok_or("Not a valid number, must be a positive integer".to_string())
+            .map(NumberArg)
+    }
+}
+
+#[derive(Clone, Debug)]
+enum ArgOrStdin<A> {
+    Arg(A),
+    Stdin,
+}
+
+impl<A> ArgOrStdin<A> {
+    fn into_or_parse_line<P, E>(self, parser: P) -> Result<Result<A, E>, io::Error>
+    where
+        P: Fn(&str) -> Result<A, E>,
+    {
+        match self {
+            ArgOrStdin::Arg(arg) => Ok(Ok(arg)),
+            ArgOrStdin::Stdin => {
+                let mut buffer = String::new();
+                stdin().read_line(&mut buffer)?;
+                Ok(parser(&buffer))
+            }
+        }
+    }
+}
+
+impl<A, E> ArgOrStdin<A>
+where
+    A: FromStr<Err = E>,
+    E: Into<String>,
+{
+    fn parse(input: &str) -> Result<Self, String> {
+        match input {
+            "-" => Ok(Self::Stdin),
+            input => A::from_str(input)
+                .map(Self::Arg)
+                .map_err(Into::into)
+                .map_err(|e| format!("{e}. Use '-' for piping from stdin.")),
+        }
+    }
 }
 
 #[derive(Debug, Parser)]
@@ -27,8 +77,8 @@ enum Command {
 #[derive(Debug, Args)]
 #[command(about = "Count and print total number of steps in the sequence starting from <NUMBER>")]
 struct Count {
-    #[arg(help = "A positive integer", value_parser = parse_number)]
-    number: Number,
+    #[arg(help = "A positive integer", value_parser = ArgOrStdin::<NumberArg>::parse)]
+    number: ArgOrStdin<NumberArg>,
 }
 
 #[derive(Debug, Args)]
@@ -37,17 +87,28 @@ struct Sequence {
     #[arg(help = "Prefix each step with the step number", long = "enumerate")]
     enumerate: bool,
 
-    #[arg(help = "A positive integer", value_parser = parse_number)]
-    number: Number,
+    #[arg(help = "A positive integer", value_parser = ArgOrStdin::<NumberArg>::parse)]
+    number: ArgOrStdin<NumberArg>,
 }
 
 fn count(args: Count) {
-    let count = collatz::sequence(args.number).count().sub(1);
+    let number = match args.number.into_or_parse_line(NumberArg::from_str) {
+        Ok(Ok(number)) => number.0,
+        Ok(Err(error)) => clap::Error::raw(clap::error::ErrorKind::Format, error).exit(),
+        Err(error) => clap::Error::raw(clap::error::ErrorKind::Io, error).exit(),
+    };
+
+    let count = collatz::sequence(number).count().sub(1);
     println!("{count}")
 }
 
 fn steps(args: Sequence) {
-    let sequence = collatz::sequence(args.number);
+    let number = match args.number {
+        ArgOrStdin::Arg(number) => number.0,
+        ArgOrStdin::Stdin => todo!(),
+    };
+
+    let sequence = collatz::sequence(number);
 
     if args.enumerate {
         sequence.enumerate().for_each(|(step, number)| {
