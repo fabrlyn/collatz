42f6c19550ffdf9a562cd1cbeb1aa9facc55c4e6
fabrlyn <robert.barlin@gmail.com>
2024-01-29 20:32:54 +0100

    Add `WithNewLine` for formatting error message

--- src/main.rs
+++ src/main.rs
@@ -71,11 +71,25 @@ trait GetOrExit<T> {
     fn get_or_exit(self) -> T;
 }
 
+trait WithNewLine {
+    fn with_new_line(self) -> Self;
+}
+
+impl WithNewLine for String {
+    fn with_new_line(self) -> Self {
+        if self.ends_with("\n") {
+            self
+        } else {
+            format!("{self}\n")
+        }
+    }
+}
+
 impl<T> GetOrExit<T> for Result<Result<T, String>, io::Error> {
     fn get_or_exit(self) -> T {
         match self {
             Ok(Ok(value)) => value,
-            Ok(Err(error)) => clap::Error::raw(clap::error::ErrorKind::Format, error).exit(),
+            Ok(Err(error)) => clap::Error::raw(clap::error::ErrorKind::Format, error.with_new_line()).exit(),
             Err(error) => clap::Error::raw(clap::error::ErrorKind::Io, error).exit(),
         }
     }
